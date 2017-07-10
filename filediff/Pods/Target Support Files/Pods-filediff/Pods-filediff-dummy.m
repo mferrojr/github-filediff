1b98b9186540d4ecacce694567fe346166a6ea56
Michael Ferro <mferrojr@gmail.com>
2017-07-09 21:45:46 -0400

    Add cocoapods with alamofire

---
+++ filediff/Pods/Target Support Files/Pods-filediff/Pods-filediff-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_filediff : NSObject
+@end
+@implementation PodsDummy_Pods_filediff
+@end
---
+++ filediff/Pods/Target Support Files/Pods-filediffUITests/Pods-filediffUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_filediffUITests : NSObject
+@end
+@implementation PodsDummy_Pods_filediffUITests
+@end
