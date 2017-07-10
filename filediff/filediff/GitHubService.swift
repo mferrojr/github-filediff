b74905a651ba7205d64927ae56d3229ab321e35b
Michael Ferro <mferrojr@gmail.com>
2017-07-09 21:38:48 -0400

    Start to organize project structure

---
+++ filediff/filediff/GitHubService.swift
@@ -0,0 +1,9 @@
+//
+//  GitHubService.swift
+//  filediff
+//
+//  Created by Michael Ferro on 7/9/17.
+//  Copyright © 2017 Michael Ferro. All rights reserved.
+//
+
+import Foundation
---
+++ filediff/filediff/PRListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PRListViewController.swift
+//  filediff
+//
+//  Created by Michael Ferro on 7/9/17.
+//  Copyright © 2017 Michael Ferro. All rights reserved.
+//
+
+import Foundation
