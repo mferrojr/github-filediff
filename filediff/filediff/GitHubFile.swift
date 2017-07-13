57dadd9697fcc44ecf4328208f0f4066fec4b272
Michael Ferro <mferrojr@gmail.com>
2017-07-13 05:50:07 -0400

    Format diff structure and begin to extract diff values

--- filediff/filediff/GitHubFile.swift
+++ filediff/filediff/GitHubFile.swift
@@ -7,7 +7,64 @@
 //
 
 import Foundation
+import UIKit
 
 struct GitHubFile {
     var name = ""
+    var groups = [GitHubFileGroup]()
+    
+    mutating func setName(value: String) {
+        name = value
+    }
+    
+    mutating func addGroup(value: GitHubFileGroup) {
+        groups.append(value)
+    }
+}
+
+struct GitHubFileGroup {
+    typealias lineDiff = (GitHubFileDiff,GitHubFileDiff)
+    
+    var title = ""
+    var diffs = [Int: lineDiff]()
+    
+    mutating func setTitle(value: String) {
+        title = value
+    }
+    
+    mutating func addDiff(key: Int, value: lineDiff) {
+        diffs[key] = value
+    }
+}
+
+struct GitHubFileDiff {
+    var type : GitHubFileDiffType = .none
+    var text = ""
+}
+
+enum GitHubFileDiffType {
+    case none, remove, add
+    
+    func getColor() -> UIColor {
+        switch self {
+        case .none:
+            return .clear
+        case .remove:
+            return UIColor(red: 0.937, green: 0.839, blue: 0.839, alpha: 1)
+        case .add:
+            return UIColor(red: 0.835, green: 0.914, blue: 0.808, alpha: 1)
+        }
+    }
+    
+    func getSymbol() -> String {
+        switch self {
+        case .none:
+            return ""
+        case .remove:
+            return "-"
+        case .add:
+            return "+"
+        }
+    }
+    
 }
--- filediff/filediff/PRDiffDataSource.swift
+++ filediff/filediff/PRDiffDataSource.swift
@@ -11,48 +11,53 @@ import UIKit
 
 class PRDiffDataSource : NSObject, UITableViewDataSource {
     
-    private(set) var files = [GitHubFile]()
+    private(set) var datas = [GitHubFile]()
     
     //MARK: - Public Functions
     func refresh(fileText: String){
         // Clear
-        files.removeAll()
+        datas.removeAll()
 
         // Clean up format
         let text = fileText.replacingOccurrences(of: "\t", with: "    ")
-        let lines = text.components(separatedBy: "\n")
+        let files = text.components(separatedBy: "diff --git ")
         
-        var file: GitHubFile?
-        for line in lines {
-            // new file
-            if line.contains("diff") {
-                // add existing file to array if there is one
-                if let file = file {
-                    files.append(file)
+        
+        for file in files.filter({ !$0.isEmpty }) {
+            var ghFile = GitHubFile()
+            var isFirstLine = true
+
+            for line in file.components(separatedBy: "\n") {
+                if isFirstLine {
+                    if let index = line.range(of: "/", options: .backwards)?.lowerBound {
+                        ghFile.setName(value: line.substring(from: line.index(index, offsetBy: 1)))
+                    }
+                    
+                    isFirstLine = false
                 }
-                file = GitHubFile()
-                
-                // Set the file name
-                if let index = line.range(of: "/", options: .backwards)?.lowerBound {
-                    file?.name = line.substring(from: line.index(index, offsetBy: 1))
+                // New Group
+                else if line.hasPrefix("@@") && line.hasSuffix("@@"){
+                    var group = GitHubFileGroup()
+                    group.setTitle(value: line)
+                    ghFile.addGroup(value: group)
                 }
             }
-            else {
-                
-            }
+            
+            datas.append(ghFile)
         }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return files.count
+        return datas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FileDiffCell.fileDiffCell.rawValue, for: indexPath) as! FileDiffTableViewCell
         
-        let model = files[indexPath.row]
+        let model = datas[indexPath.row]
         
-        cell.configure(FileDiffTableViewModel(name: model.name))
+        cell.configure(FileDiffTableViewModel(name: model.name, groups: model.groups))
         
         return cell
     }
