f6f85b0350eb6cbe7f855025bbe6fc91473d45b0
Michael Ferro <mferrojr@gmail.com>
2024-12-13 15:01:06 -0500

    Use property wrapper to inject services

---
+++ filediff/Utils/DependencyPropertyWrapper.swift
@@ -0,0 +1,9 @@
+//
+//  DependencyPropertyWrapper.swift
+//  PR Diff Tool
+//
+//  Created by Michael Ferro, Jr. on 12/6/24.
+//  Copyright © 2024 Michael Ferro. All rights reserved.
+//
+
+import Foundation
